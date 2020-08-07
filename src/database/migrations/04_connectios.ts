73e4dde006556769e551f8d140e411f885f4da99
Leonidas Amorim <leonidasamorim@gmail.com>
2020-08-07 13:09:49 -0300

    finish create migrations

--- src/database/migrations/04_connectios.ts
+++ src/database/migrations/04_connectios.ts
@@ -2,10 +2,9 @@ import Knex from 'knex';
 
 export async function up(knex: Knex) {
 
-    return knex.schema.createTable('classes', table => {
+    return knex.schema.createTable('connections', table => {
         table.increments('id').primary();
-        table.string('subject').notNullable();
-        table.decimal('cost').notNullable();
+        
 
         table.integer('user_id')
         .notNullable()
@@ -13,10 +12,14 @@ export async function up(knex: Knex) {
         .inTable('users')
         .onUpdate('CASCADE')
         .onDelete('CASCADE');
+
+        table.timestamp('created_at')
+        .defaultTo('now()')
+        .notNullable();
     })
 
 }
 
 export async function down(knex: Knex) {
-    return knex.schema.dropTable('classes');
+    return knex.schema.dropTable('connections');
 }
